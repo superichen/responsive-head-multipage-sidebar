2264a2a2641143fdd40401434db70268f155e7fd
superichen <121595091+superichen@users.noreply.github.com>
2023-10-31 12:02:46 +0800

    Initial commit

---
+++ src/components/Home/collaboration/Pnb.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pnb = () => {
+    return (
+        <div className="pnb-main">
+            <div className="collab-content">
+                <p>We were fortunate and blessed to be sponsored by "Punjab National Bank" (PNB). Their invaluable support has enabled us to pursue our goals and endeavors, fostering growth and success. We are grateful for PNB's commitment to empowering individuals and organizations, and we deeply value their partnership in making a positive impact.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Pnb
